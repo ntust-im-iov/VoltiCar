5513ae803030539cf79ecb08e57e7522976cc6e0
RongjayWu <rjw20031128@gmail.com>
2025-04-15 17:58:50 +0800

    建立自適應元件原型

---
+++ lib/shared/widgets/adaptive_component.dart
@@ -0,0 +1,45 @@
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flutter/material.dart';
+import 'package:volticar_app/views/home/garage_view.dart';
+
+class AdaptiveComponent extends SpriteComponent
+    with TapCallbacks, HasGameRef<VoltiCarGame> {
+  AdaptiveComponent(
+      this.screenSize,
+      this.widthGain,
+      this.heightGain,
+      this.imagePath,
+      this.positionGainofX,
+      this.positionGainofY,
+      this.onPressed)
+      : super(anchor: Anchor.bottomCenter);
+
+  final String imagePath;
+  final Vector2 screenSize;
+  final double widthGain;
+  final double heightGain;
+  final double positionGainofX;
+  final double positionGainofY;
+  final VoidCallback? onPressed;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    sprite = await gameRef.loadSprite(imagePath);
+
+    // Calculate adaptive size and position
+    final double componentWith = screenSize.x * widthGain;
+    final double componentHeight = componentWith * heightGain;
+    size = Vector2(componentWith, componentHeight);
+
+    // Position the component at the horizontal center and a dynamic position from the bottom
+    position =
+        Vector2(screenSize.x * positionGainofX, screenSize.y * positionGainofY);
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    onPressed?.call(); // Execute the callback on tap
+  }
+}
